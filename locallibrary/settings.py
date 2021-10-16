8e7b34af5264cc1da483d274e471b923bd12244a
laurapcc <lauradepazc@gmail.com>
2021-10-16 21:41:43 +0200

    added allowed hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -22,16 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-juqz)$efq-l%(jlc17(bxkmi9j9fowz&%m7-r!0xs6ek7*2@&y'
+# SECRET_KEY = 'django-insecure-juqz)$efq-l%(jlc17(bxkmi9j9fowz&%m7-r!0xs6ek7*2@&y'
 SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY',
     'django-insecure-juqz)$efq-l%(jlc17(bxkmi9j9fowz&%m7-r!0xs6ek7*2@&y')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
+# DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['safe-citadel-40470.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -94,9 +94,9 @@ if os.getenv('SQLITE', False):
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 else:
-    import dj_database_url
-    DATABASES['default'] = dj_database_url.config(default='postgres://alumnodb:alumnodb@localhost:5432/psi',
-                                                  conn_max_age=500)
+    DATABASES['default'] = dj_database_url.config(
+        default='postgres://alumnodb:alumnodb@localhost:5432/psi',
+        conn_max_age=500)
 
 
 # Password validation
@@ -150,9 +150,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # The following environment variable, called DATABASE_URL, has to be defined
 # at the o.s. level: export DATABASE_URL =
 # 'postgres://alumnodb:alumnodb@localhost:5432/psi'
-db_from_env = \
-    dj_database_url.config(default='postgres://alumnodb:alumnodb@localhost:5432/psi',
-                           conn_max_age=500)
+db_from_env = dj_database_url.config(
+    default='postgres://alumnodb:alumnodb@localhost:5432/psi',
+    conn_max_age=500)
 
 DATABASES['default'].update(db_from_env)
 
